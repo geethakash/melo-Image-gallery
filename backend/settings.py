d1c2967e3a7177a6b986c14f198351953710ceb8
Akash Geethanjana <akashgeethanjana@gmail.com>
2022-03-21 15:59:45 +0530

    🚑️:fix: collectstatic

--- backend/settings.py
+++ backend/settings.py
@@ -174,12 +174,12 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    BASE_DIR/'static',
+    # BASE_DIR/'static',
     BASE_DIR/'frontend/build/static'
 ]
 
 MEDIA_ROOT = 'static/images'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -195,7 +195,7 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get("API_SECRET")
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 if os.getcwd() == '/app':
